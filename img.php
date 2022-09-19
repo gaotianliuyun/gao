49475a29bd75b6d330e98e68c5670e7226fa4510
chuqiuyu <254586273@qq.com>
2022-09-19 09:51:53 +0800

    上传新文件

---
+++ img.php
@@ -0,0 +1,9 @@
+﻿<?php
+/**
+* PHP随机图显示
+*/
+header('Content-Type: text/html; charset=UTF-8');
+$img_array = glob("./img/*.jpg",GLOB_BRACE);
+$img = array_rand($img_array);
+header("location:.$img_array[$img]");
+?>
