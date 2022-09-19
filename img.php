79c09c08d9749cf1127fbb439c05a2426e994545
chuqiuyu <254586273@qq.com>
2022-09-19 10:10:23 +0800

    Update img.php

--- img.php
+++ img.php
@@ -1,9 +1,9 @@
-﻿<?php
+<?php
 /**
 * PHP随机图显示
 */
 header('Content-Type: text/html; charset=UTF-8');
-$img_array = glob("./img/*.jpg",GLOB_BRACE);
+$img_array = glob("https://gitcode.net/chuqiuyu/chuqiuyu/-/raw/master/img/*.jpg",GLOB_BRACE);
 $img = array_rand($img_array);
 header("location:.$img_array[$img]");
 ?>
