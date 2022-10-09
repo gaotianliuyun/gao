1e9f005e5c12f10e9cb4c800c4b784c7a726fc5b
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-09 12:52:29 +0800

    Create 真不卡.js

---
+++ js/真不卡.js
@@ -0,0 +1,8 @@
+muban.首图2.二级.tabs = '.stui-pannel__head.bottom-line.active.clearfix h3';
+var rule = Object.assign(muban.首图2,{
+title:'真不卡',
+host:'https://www.zbkk.net',
+url:'/vodshow/fyclass--------fypage---.html',
+class_parse:'.stui-header__menu .dropdown li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
+// searchUrl:'/vodsearch/**----------fypage---.html',
+});
