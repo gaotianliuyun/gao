23eece8d00c628462277d049810c77de7452ed71
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-21 19:04:40 +0800

    Delete vip影院.js

--- vip影院.js
+++
@@ -1,6 +0,0 @@
-var rule = Object.assign(muban.vfed,{
-title:'VIP影院',
-host:'http://360yy.cn',
-url:'/index.php/vod/show/id/fyclass/page/fypage.html',
-searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
-});
