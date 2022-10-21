1145f8056c36994575697b438296c03c0bc33958
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-21 19:02:03 +0800

    Delete Nike影视.js

--- Nike影视.js
+++
@@ -1,8 +0,0 @@
-var rule = Object.assign(muban.mxpro,{
-title:'Nike影视',
-host:'https://www.ajeee.com',
-url:'/show/fyclass/page/fypage.html',
-class_name:'电影&电视剧&综艺&动漫',
-class_url:'Movie&Tv&Variety&Cartoon',
-class_parse:'',
-});
