b039640da661379901e8b9f196f695cdb277e82f
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-21 19:10:11 +0800

    Delete 番茄影视.js

--- 番茄影视.js
+++
@@ -1,6 +0,0 @@
-var rule = Object.assign(muban.海螺3,{
-title:'番茄影视',
-host:'https://www.tjomet.com',
-url:'/vodshow/fyclass--------fypage---.html',
-searchUrl:'/vodsearch/**----------fypage---.html',
-});
