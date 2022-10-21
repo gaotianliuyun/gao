94e4d68c8dc5ad4f17464731d75052aadbbfad40
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-21 19:09:18 +0800

    Delete 蓝光影院.js

--- 蓝光影院.js
+++
@@ -1,7 +0,0 @@
-var rule = Object.assign(muban.mxpro,{
-title:'蓝光影院',
-host:'https://lgyy.cc',
-url:'/vodshow/fyclass--------fypage---.html',
-searchUrl:'/vodsearch/**-------------.html',
-class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
-});
