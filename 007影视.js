d5f2bc06e8853ec9a5ed6b8386c90e2022b2930c
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-21 18:47:59 +0800

    Delete 007影视.js

--- 007影视.js
+++
@@ -1,7 +0,0 @@
-muban.首图2.二级.tabs = '.nav-tabs.dpplay&&li';
-var rule = Object.assign(muban.首图2,{
-    title:'007影视',
-    host:'https://www.007ts.me',
-    url:'/channel/fyclass-fypage.html',
-    searchUrl:'/search/**----------fypage---.html',
-});
