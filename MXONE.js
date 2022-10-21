177669732f0580b9bd399ed966839ae177ce618c
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-21 19:02:23 +0800

    Delete MXONE.js

--- MXONE.js
+++
@@ -1,9 +0,0 @@
-var rule = Object.assign(muban.mxpro,{
-	title:'MXONE',
-    host:'https://www.jpys.me',
-    url:'vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html/',
-    class_name:'电影&电视剧&动漫&综艺',
-    class_url:'1&2&3&4',
-	class_parse:'',
-});
