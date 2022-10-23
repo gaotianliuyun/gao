efb13dc3758e7aa11342da14d8c3f89f0fcef306
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-23 09:13:43 +0800

    Add files via upload

---
+++ js/量子影视.js
@@ -0,0 +1,8 @@
+var rule= {
+模板:'mxone5',
+title:'量子影视',
+host:'http://www.lzizy9.com',
+url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+searchUrl:'/index.php/vod/search.html?wd=**',
+cate_exclude:'网址'
+}
