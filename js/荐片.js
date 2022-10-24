df372b7a0b4c16511ad6c5fdd4b82f37522b4c8a
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-24 08:15:55 +0800

    Add files via upload

--- js/荐片.js
+++ js/荐片.js
@@ -33,6 +33,7 @@ var rule = {
     }],
     lazy:'',
     double:true,
+    图片来源:'@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',
 	推荐:'json:data;room;*;cover;*;*',
 	一级:'json:data;title;path;playlist.title;id',
     二级:{
