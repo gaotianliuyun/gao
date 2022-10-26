7e48bf968091a9f870e48824e406d3c2a6723291
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-26 10:25:24 +0800

    Add files via upload

---
+++ js/豆芽影院.js
@@ -0,0 +1,7 @@
+
+var rule = Object.assign(muban.mxpro,{
+title:'豆芽影院',
+host:'https://www.imdy.tv',
+class_parse:'.navbar-items&&li;a&&Text;a&&href;.*/(.*?).html',
+cate_exclude: '追剧周表|今日更新0|热搜',
+});
