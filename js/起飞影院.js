5bd1c329bd01e8d3879b93b6f8fd0f75413704b0
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-10-31 10:32:42 +0800

    Add files via upload

--- js/起飞影院.js
+++ js/起飞影院.js
@@ -1,4 +1,4 @@
-﻿var rule = {
+var rule = {
     title:'起飞影院',
     host:'http://www.qfitv.com',
     // homeUrl:'/',
@@ -11,14 +11,15 @@
         'User-Agent':'MOBILE_UA',
         // "Cookie": "searchneed=ok"
     },
-    class_name:'电影&电视剧&综艺&动漫&纪录片',
-    class_url:'1&2&3&4&55',
+    class_name:'电影&电视剧&综艺&动漫&纪录片&喜剧片&爱情片&同性片&剧情片&国产剧&欧美剧&泰剧',
+    class_url:'1&2&3&4&55&7&8&23&11&13&16&33',
     play_parse:true,
     lazy:'',
     limit:6,
     推荐:'.module-items;.module-poster-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
     double:true, // 推荐内容是否双层定位
     一级:'.module-poster-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
-    二级:{"title":"h1&&Text;.module-info-content&&.module-info-item:eq(4)&&.module-info-item-content&&Text","img":".ls-is-cached.lazy.lazyload&&data-original","desc":".text-muted:eq(-1)&&Text;.text-muted:eq(-1)&&Text;.text-muted:eq(-1)&&Text;.module-info-content&&.module-info-item:eq(6)&&.module-info-item-content&&Text","content":".show-desc&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-play-list-content:eq(0) a"},
+    二级:{"title":"h1&&Text;.module-info-tag-link:eq(2)&&Text","img":".ls-is-cached.lazy.lazyload&&data-original","desc":".module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(-2)&&Text;.module-info-content&&.module-info-item:eq(2)&&.module-info-item-content&&Text;.module-info-content&&.module-info-item:eq(1)&&.module-info-item-content&&Text","content":".module-info-introduction-content&&Text","tabs":".module-tab-items-box:eq(0)&&.module-tab-item","lists":".module-play-list-content:eq(0) a"},
     搜索:'.module-card-item.module-item;.module-card-item-title&&Text;img&&data-original;.module-item-note&&Text;a.play-btn-o&&href',
 }
+
