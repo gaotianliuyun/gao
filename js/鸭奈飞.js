8664787e819e3e6c92c603495fb55a0824d153d1
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-11-07 16:12:09 +0800

    Add files via upload

--- js/鸭奈飞.js
+++ js/鸭奈飞.js
@@ -1,6 +1,9 @@
 var rule = Object.assign(muban.mxpro,{
 title:'鸭奈飞',
-host:'https://yanetflix.com',
+//host:'https://yanetflix.com',
+host:'https://yanetflix.tv/',
 url:'/vodshow/fyclass--------fypage---.html',
-class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
+//class_parse:'.navbar-items&&li;a&&Text;a&&href;.*/(.*?).html',
+class_name:'电视剧&电影&综艺&动漫',
+class_url:'lianxuju&dianying&zongyi&dongman',
 });
