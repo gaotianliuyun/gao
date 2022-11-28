2f86e55ee5d33d9b0c15ebab4dea9507721363e8
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-11-28 18:37:44 +0800

    Add files via upload

--- js/乐猪TV.js
+++ js/乐猪TV.js
@@ -3,11 +3,13 @@ var rule = {
     host:'http://www.lezhutv.com',
     // homeUrl:'/',
     url:'/type/fyclass-fypage.html',
-    searchUrl:'/search-pg-fypage-wd-**.html',
+    // searchUrl:'/search-pg-fypage-wd-**.html',
+    searchUrl:'/index.php?m=vod-search#wd=**&search=;post',
     searchable:2,
     quickSearch:0,
     headers:{
-        'User-Agent':'UC_UA'
+        // 'User-Agent':'UC_UA'
+        'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
     class_parse:'div.nav a;a&&Text;a&&href;/(\\d.+).html',
@@ -17,8 +19,7 @@ var rule = {
     推荐:'.tbox2;*;*;*;*;*',
     double:true, // 推荐内容是否双层定位
     一级:'ul.tbox_m2 li;a&&title;a&&data-original;span&&Text;a&&href',
-    二级:{"title":".data h4&&Text;.yac&&Text","img":".item-lazy&&data-original","desc":";;;.act&&Text;.dir&&Text","content":".tbox_js&&Text","tabs":".tbox_t&&h3","lists":".list_block.show:eq(#id) li"},
-    //二级:{"title":".data h4--i&&Text;.yac&&Text","img":".item-lazy&&data-original","desc":";;;.act&&Text;.dir&&Text","content":".tbox_js&&Text","tabs":"js:pdfa=jsp.pdfa;TABS=pdfa(html,'.tbox_t h3').map(function(it,idex){return '线路'+(idex+1)})","lists":"ul.list_block:eq(#id) li"},
+    二级:{"title":".data h4--i&&Text;.yac&&Text","img":".item-lazy&&data-original","desc":";;;.act&&Text;.dir&&Text","content":".tbox_js&&Text","tabs":"js:pdfa=jsp.pdfa;TABS=pdfa(html,'.tbox_t h3').map(function(it,idex){return '线路'+(idex+1)})","lists":"ul.list_block:eq(#id) li","tabs":".tbox_t h3"},
     搜索:'ul.tbox_m li;*;*;*;*',
 	
 }
