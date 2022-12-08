e7c558289adf06fbee6c6306e6b10b6138e9eae7
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-12-08 19:44:48 +0800

    Add files via upload

--- js/JRKAN直播.js
+++ js/JRKAN直播.js
@@ -11,7 +11,7 @@
 
 var rule = {
     title:'JRKAN直播',
-    host:'http://www.jrkan365.com',
+    host:'http://www.jrkan365.com/?lan=1',
 	// JRKAN备用域名:jrkan365.com / jrkankan.com / jryyds.com / jrsbxj.com
 	// JRKAN网址发布:qiumi1314.com
     url:'/fyclass',
