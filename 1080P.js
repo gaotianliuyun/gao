14fcde53dcc27c1f1f1f344054fe597258a2e8c1
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2022-12-08 19:16:14 +0800

    Delete 1080P.js

--- 1080P.js
+++
@@ -1,7 +0,0 @@
-var rule = Object.assign(muban.mxpro,{
-title:'1080P',
-host:'https://1080p.tv',
-url:'/vodshow/fyclass--------fypage---/',
-searchUrl:'/vodsearch/**----------fypage---/',
-class_parse:'.navbar-items li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?)/',
-});
