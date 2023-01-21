6788282affc31c9d8d4dd39bdff0fff575647689
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-01-21 16:17:05 +0800

    Delete 小猫咪.js

--- 小猫咪.js
+++
@@ -1,9 +0,0 @@
-muban.海螺3.二级.desc = ';;;.hl-full-box .hl-col-xs-12:eq(3)&&Text;.hl-full-box .hl-col-xs-12:eq(4)&&Text';
-muban.海螺3.二级.content = '.hl-col-xs-12.blurb&&Text';
-var rule = {
-    title:'小猫咪',
-    模板:'海螺3',
-    // host:'https://xmaomi.net',
-    host:'https://xmaomi.top',
-    url: '/vod___________show/fyclass--------fypage---.html',
-}
