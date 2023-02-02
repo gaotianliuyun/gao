fb9d3db0a1c8a2e8ad2c2ff04f7cfd02c00a9810
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-02-02 08:22:54 +0800

    Add files via upload

--- js/8号影院.js
+++ js/8号影院.js
@@ -1,22 +1,21 @@
+muban.首图2.二级.desc = ';;;.data--span:eq(0)&&Text;.data--span:eq(1)&&Text';
+muban.首图2.二级.tabs = '.stui-pannel__head h3';
 var rule={
-title:'8号影院',
-    host:'http://www.8hysw.com',
-    // homeUrl:'/',
-    url:'/frim/fyclass-fypage.html',
-    searchUrl:'/search.php?page=fypage&searchword=**&searchtype=',
-searchable:2,//是否启用全局搜索,
-quickSearch:0,//是否启用快速搜索,
-filterable:0,//是否启用分类筛选,
-class_name:'电影&电视剧&综艺&动漫&日韩剧&国产剧&欧美剧&港台剧',
-    class_url:'1&2&3&4&16&13&15&14',
-play_parse:true,
-lazy:'',
-limit:6,
-tab_exclude:'本周热门|最近更新',
-推荐:'.stui-pannel_bd;.stui-vodlist li;h4&&Text;.lazyload&&data-original;.text-right&&Text;a&&href',
-double:true, // 推荐内容是否双层定位
-一级:'.stui-vodlist.clearfix&&li;a&&title;.lazyload&&data-original;.text-right&&Text;a&&href',
-二级:{"title":"h1&&Text;.stui-content__detail&&p&&Text","img":".lazyload&&data-original","desc":".data:eq(0)&&Text;.data:eq(1)&&Text;.data:eq(2)&&Text;.data:eq(3)&&Text","content":".desc&&Text","tabs":".stui-pannel__head.bottom-line h3","lists":".stui-content__playlist:eq(#id) li"},
- 搜索:muban.首图2.搜索2,
-
+	title:'8号影院',
+	模板:'首图2',
+	host:'http://www.8hysw.com',
+	// url:'/frim/fyclass-fypage.html',
+	url:'/frim/fyfilter.html',
+	filterable:1,//是否启用分类筛选,
+	filter_url:'{{fl.cateId}}-fypage',
+	filter: {"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作","v":"5"},{"n":"爱情","v":"6"},{"n":"科幻","v":"7"},{"n":"恐怖","v":"8"},{"n":"喜剧","v":"10"},{"n":"剧情","v":"12"},{"n":"在线直播","v":"28"}]}],"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"13"},{"n":"港台剧","v":"14"},{"n":"欧美剧","v":"15"},{"n":"日韩剧","v":"16"}]}]},
+	filter_def:{
+		1:{cateId:'1'},
+		2:{cateId:'2'},
+		3:{cateId:'3'},
+		4:{cateId:'4'}
+	},
+	searchUrl:'/search.php?page=fypage&searchword=**&searchtype=',
+	class_parse: '.stui-header__menu li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
+	搜索:muban.首图2.搜索2,
 }
