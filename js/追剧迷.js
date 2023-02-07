67fc272d3414e1dd328c33e4697b8bda8bd19c1f
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-02-07 18:53:30 +0800

    Add files via upload

--- js/追剧迷.js
+++ js/追剧迷.js
@@ -1,14 +1,20 @@
-var rule = Object.assign(muban.vfed,{
-title:'追剧迷',
-host:'https://www.zhuijumi.cc',
-url:'/videotype//fyclass--------fypage---.html',
-searchUrl:'/vodsearch/**-fypage.html',
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    推荐:'ul.fed-list-info.fed-part-rows;li;a.fed-list-title&&Text;a&&data-original;.fed-list-remarks&&Text;a&&href',
-    double:true, // 推荐内容是否双层定位
-    一级:'.fed-list-info&&li;a.fed-list-title&&Text;a&&data-original;.fed-list-remarks&&Text;a&&href',
-    二级:{"title":"h1&&Text;.fed-mute&&Text","img":".fed-list-info&&a&&data-original","desc":".fed-deta-content&&.fed-part-rows&&li:eq(-1)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-2)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(-3)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(0)&&Text;.fed-deta-content&&.fed-part-rows&&li:eq(1)&&Text","content":".fed-conv-text&&Text","tabs":".fed-tabs-foot&&.fed-part-rows li","lists":".fed-deta-playlist&&.fed-tabs-btm:eq(#id) li"},
-    搜索:'.fed-list-deta;h1&&Text;.fed-lazy&&data-original;.fed-list-remarks&&Text;a&&href;.fed-deta-content&&Text',
-});
+var rule = {
+    title:'追剧迷',
+    模板:'vfed',
+    host:'https://www.zhuijumi.cc',
+    url:'/videotype/fyclass-fypage.html',
+    searchUrl:'/vodsearch/**-fypage.html',
+    class_parse: '.fed-part-tips li;a&&Text;a&&href;.*/(.*?).html',
+    cate_exclude:'更多|申请',
+	lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+    二级: {
+        "title": "h1&&Text;.fed-col-xs6--span:eq(0)&&Text",
+        "img": ".fed-list-info&&a&&data-original",
+        "desc": ".fed-col-xs12.fed-part-eone:eq(3)&&Text;;;.fed-col-xs12.fed-part-eone--span:eq(0)&&Text;.fed-col-xs12.fed-part-eone--span:eq(1)&&Text",
+        "content": ".fed-conv-text:eq(0)&&Text",
+        "tabs": "ul.fed-padding&&li",
+        "lists": ".fed-tabs-btm:eq(#id) li"
+    },
+    搜索: '.fed-list-deta;h1&&Text;.fed-lazy&&data-original;.fed-list-remarks&&Text;a&&href;.fed-col-xs12.fed-part-eone:eq(2)&&Text',
+
+}
