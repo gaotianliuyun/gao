841aa995e135287f09d636a67e6eadc75687482c
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-02-17 12:49:13 +0800

    Delete TV云播.js

--- TV云播.js
+++
@@ -1,9 +0,0 @@
-muban.首图.二级.tabs = '.myui-panel__head.bottom-line h3';
-var rule = Object.assign(muban.首图,{
-title:'TV云播',
-host:'http://www.tvyb03.com',
-url:'/vod/type/id/fyclass/page/fypage.html',
-class_parse:'.item.nav-list.clearfix li:gt(0):lt(5);a&&Text;a&&href;/(\\d+).html',
-推荐:'.myui-panel_bd.clearfix;.myui-vodlist.clearfix&&li;a&&title;a&&data-original;.background-color&&Text;a&&href',
- 一级:'.myui-vodlist li;a&&title;a&&data-original;.pic-tag-top&&Text;a&&href',
-});
