869f01644d4e218b042300d2879c73a1e031e22e
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-03-17 22:55:38 +0800

    Add files via upload

--- js/蓝光影院.js
+++ js/蓝光影院.js
@@ -3,7 +3,9 @@ muban.mxpro.二级.desc = ';;;.module-info-item-content:eq(1)&&Text;.module-info
 var rule = {
 	title:'蓝光影院',
 	模板:'mxpro',
-	host:'https://www.lgyy.tv',
+	// host:'https://www.lgyy.tv',
+    host:'https://www.lgyy.vip',
+    // host:'https://www.6446.tv',
 	// url:'/vodshow/fyclass--------fypage---.html',
 	url:'/vodshow/fyfilter.html',
     filterable:1,//是否启用分类筛选,
@@ -23,5 +25,13 @@ var rule = {
         4:{cateId:'4'}
     },
 	searchUrl:'/vodsearch/**----------fypage---.html',
-	class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
+    二级: {
+        "title": "h1&&Text;.module-info-tag&&Text",
+        "img": ".lazyload&&data-original",
+        "desc": ";;;.module-info-item-content:eq(1)&&Text;.module-info-item-content:eq(0)&&Text",
+        "content": ".module-info-introduction&&Text",
+        "tabs": ".module-tab-item",
+        "lists": ".module-play-list:eq(#id) a"
+    },
+	class_parse:'.navbar-items li:gt(1):lt(7);a&&Text;a&&href;.*/(.*?).html',
 }
