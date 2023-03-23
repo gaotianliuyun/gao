4d65f485665fe9d0b23a83f11bafc872cead474f
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-03-23 10:56:14 +0800

    Add files via upload

--- js/哔哩兔.js
+++ js/哔哩兔.js
@@ -1,7 +1,6 @@
-muban.首图2.二级.desc = '.data:eq(1)&&Text;;;.data:eq(6)&&Text;.data:eq(5)&&Text';
 var rule = {
 	title:'哔哩兔',
-	模板:'首图2',
+	// 模板:'首图2',
 	host:'https://www.bilituys.com',
 	url:'/bilishow/fyclassfyfilter.html',
 	filterable:1,//是否启用分类筛选,
@@ -13,8 +12,19 @@ var rule = {
 		"4":[{"key":"class","name":"剧情","value":[{"n":"全部","v":""},{"n":"情感","v":"情感"},{"n":"科幻","v":"科幻"},{"n":"热血","v":"热血"},{"n":"推理","v":"推理"},{"n":"搞笑","v":"搞笑"},{"n":"冒险","v":"冒险"},{"n":"萝莉","v":"萝莉"},{"n":"校园","v":"校园"},{"n":"动作","v":"动作"},{"n":"机战","v":"机战"},{"n":"运动","v":"运动"},{"n":"战争","v":"战争"},{"n":"少年","v":"少年"},{"n":"少女","v":"少女"},{"n":"社会","v":"社会"},{"n":"原创","v":"原创"},{"n":"亲子","v":"亲子"},{"n":"益智","v":"益智"},{"n":"励志","v":"励志"},{"n":"其他","v":"其他"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"大陆"},{"n":"日本","v":"日本"},{"n":"美国","v":"美国"},{"n":"韩国","v":"韩国"},{"n":"香港","v":"香港"},{"n":"台湾","v":"台湾"},{"n":"泰国","v":"泰国"},{"n":"英国","v":"英国"},{"n":"新加坡","v":"新加坡"},{"n":"其他","v":"其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"2023"},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010","v":"2010"},{"n":"2009","v":"2009"},{"n":"2008","v":"2008"},{"n":"2007","v":"2007"},{"n":"2006","v":"2006"},{"n":"2005","v":"2005"},{"n":"2004","v":"2004"},{"n":"2003","v":"2003"},{"n":"2002","v":"2002"},{"n":"2001","v":"2001"},{"n":"2000","v":"2000"}]},{"key":"lang","name":"语言","value":[{"n":"全部","v":""},{"n":"国语","v":"国语"},{"n":"日语","v":"日语"},{"n":"英语","v":"英语"},{"n":"粤语","v":"粤语"},{"n":"闽南语","v":"闽南语"},{"n":"韩语","v":"韩语"},{"n":"其它","v":"其它"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}]
 	},
 	searchUrl:'/bilisch.html#wd=**&submit=;post',
-	class_parse: '.stui-header__menu li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
-	lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
-	// 搜索: 'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href;.text-muted:eq(-1)&&Text',
-	搜索: muban.首图2.搜索1,
+	searchable: 2,
+	quickSearch: 0,
+	headers: {
+		'User-Agent': 'MOBILE_UA',
+	},
+	// class_parse: '.stui-header__menu li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
+	class_name:'电影&电视剧&综艺&动漫',
+	class_url:'1&2&3&4',
+	play_parse: true,
+	// lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+	limit: 6,
+	推荐: '*',
+	一级: 'js:var d=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input,{withHeaders:true});let json=JSON.parse(html);let setCk=Object.keys(json).find((it)=>it.toLowerCase()==="set-cookie");let cookie=setCk?json[setCk].split(";")[0]:"";fetch_params.headers.Cookie=cookie;fetch_params.headers.Referer=MY_URL;html=JSON.parse(html).body;if(/检测中/.test(html)){html=request(input+"?btwaf"+html.match(/btwaf(.*?)\"/)[1],fetch_params)}let list=pdfa(html,".stui-vodlist&&li");list.forEach((it)=>{d.push({title:pdfh(it,"a&&title"),pic_url:pd(it,"a&&data-original"),desc:pdfh(it,".pic-text&&Text"),url:pd(it,"a&&href"),})});setResult(d);',
+	二级: 'js:pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;VOD={};var html=request(input,{withHeaders:true});let json=JSON.parse(html);let setCk=Object.keys(json).find((it)=>it.toLowerCase()==="set-cookie");let cookie=setCk?json[setCk].split(";")[0]:"";fetch_params.headers.Cookie=cookie;html=JSON.parse(html).body;if(/检测中/.test(html)){html=request(input+"?btwaf"+html.match(/btwaf(.*?)\"/)[1],fetch_params)}VOD.vod_name=pdfh(html,"h1&&Text");VOD.type_name=pdfh(html,".data.hidden-xs:eq(1)&&Text");VOD.vod_pic=pd(html,".lazyload&&data-original");VOD.vod_remarks=pdfh(html,".data:eq(1)&&Text");VOD.vod_year=pdfh(html,".data.visible-xs:eq(2)&&Text");VOD.vod_area=pdfh(html,".data.visible-xs:eq(0)&&Text");VOD.vod_actor=pdfh(html,".data:eq(6)&&Text").replace("主演：","");VOD.vod_director=pdfh(html,".data:eq(5)&&Text&&Text").replace("导演：","");VOD.vod_content=pdfh(html,".detail-content&&Text");let playFrom=[];let vod_tab_list=[];let tabs=pdfa(html,"body&&h3.title");tabs.forEach((it)=>{playFrom.push(pdfh(it,"body&&Text"))});for(let i=0;i<playFrom.length;i++){let p1=".stui-content__playlist:eq(#id)&&li".replaceAll("#id",i);let new_vod_list=[];let vodList=[];try{vodList=pdfa(html,p1)}catch(e){}for(let i=0;i<vodList.length;i++){let it=vodList[i];new_vod_list.push(pdfh(it,"body&&Text").trim()+"$"+pd(it,"a&&href"))}let vlist=new_vod_list.join("#");vod_tab_list.push(vlist)}VOD.vod_play_from=playFrom.join("$$$");VOD.vod_play_url=vod_tab_list.join("$$$");',
+	搜索: '*',
 }
