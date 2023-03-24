69ece7576f5d1cc0a0f44c27705a93dadc807263
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-03-24 22:01:12 +0800

    Add files via upload

--- js/酷奇MV.js
+++ js/酷奇MV.js
@@ -3,11 +3,10 @@ var rule = {
     host:'https://www.kuqimv.com',
     homeUrl:'/play/',
     url:'/play/fyclass_fypage.html',
-    searchUrl:'/search.php?key=**',
+    searchUrl:'/search.php?key=**&pages=fypage',
     searchable:2,
     quickSearch:0,
-    // class_parse:'.m_bor li;a&&Text;a&&href;/play/(\\d+)_1.html',
-    class_parse:'.main:eq(1)&&li;a&&Text;a&&href;/play/(\\d+)_1.html',
+    class_parse:'.main:eq(1)&&li;a&&Text;a&&href;.*/(\\d+)_1.html',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
@@ -17,10 +16,7 @@ var rule = {
     limit:6,
     double:false,
     推荐:'*',
-    // 一级:'.mv_list li;.name&&Text;.pic img&&src;.singer&&Text;a&&href',
-    // 一级:'js:var d=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);let list=pdfa(html,".video_list&&li");list.forEach((it)=>{let urll=pd(it,"a&&href");let pic_urll=pd(request(urll),".pic:eq(0)&&img&&src");d.push({title:pdfh(it,".name&&Text"),desc:"🎤"+pdfh(it,".singer&&Text"),pic_url:pic_urll,url:urll})});setResult(d);',
-    一级:'js:var d=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);let list=pdfa(html,".video_list&&li");list.forEach((it)=>{d.push({title:pdfh(it,".name&&Text"),desc:"🎤"+pdfh(it,".singer&&Text"),pic_url:"https://www.kuqimv.com/user/static/images/cover/malbum.jpg",url:pd(it,"a&&href")})});setResult(d);',
+    一级:'js:var d=[];pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;var html=request(input);let list=pdfa(html,".mv_list&&li");list.forEach((it)=>{d.push({title:pdfh(it,".name&&Text"),desc:"🎤"+pdfh(it,".singer&&Text")+"｜"+pdfh(it,".time&&Text"),pic_url:pd(it,"img&&src"),url:pd(it,"a&&href"),})});setResult(d);',
     二级:'*',
-    // 搜索:'.play_xg li;*;*;*;*',
-    搜索:'*',
+    搜索:'.video_list&&li;a&&title;;.singer&&Text;a&&href;.t_03:eq(1)&&Text',
 }
