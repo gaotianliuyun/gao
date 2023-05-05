4dac5d91ea5c55c9e9f875052f5477c22c468893
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-05-05 23:14:28 +0800

    Add files via upload

--- js/七年影视.js
+++ js/七年影视.js
@@ -1,8 +1,7 @@
 var rule = {
     title:'七年影视',
     模板:'首图2',
-    // host:'https://www.vtuapp.com',
-    host:'https://www.qinian.fun',
+    host:'https://www.vtuapp.com',
     // url:'/vodshow/fyclass--------fypage---.html',
     url:'/vodshow/fyfilter.html',
     filterable:1,//是否启用分类筛选,
