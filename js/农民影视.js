35b24c801a633e88de58eb9da2ed644e54ae9fd4
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-05-16 21:18:55 +0800

    Add files via upload

--- js/农民影视.js
+++ js/农民影视.js
@@ -1,6 +1,8 @@
+// 发布页 https://www.nmdvd.com/
 var rule={
 	title:'农民影视',
-	host:'https://v.xiangdao.me',
+	// host:'https://v.xiangdao.me',
+	host:'https://www.nmddd.com',
 	url:'/vod-list-id-fyfilter.html',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}-pg-fypage-order--by-{{fl.by}}-class-0-year-{{fl.year}}-letter--area-{{fl.area}}-lang-',
