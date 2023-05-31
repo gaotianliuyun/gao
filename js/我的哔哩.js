8dd2552d129c95bbd7447f0d34c0b0cf314db029
gaotianliuyun <58679624+gaotianliuyun@users.noreply.github.com>
2023-05-31 10:30:04 +0800

    Add files via upload

--- js/我的哔哩.js
+++ js/我的哔哩.js
@@ -107,7 +107,7 @@ var rule = {
         "User-Agent":"PC_UA",
         "Referer": "https://www.bilibili.com",
         // "Cookie":"$bili_cookie"
-        "Cookie":"https://ghproxy.com/https://github.com/FongMi/CatVodSpider/raw/main/txt/cookie.txt"
+        "cookie": "buvid3=8B57D3BA-607A-1E85-018A-E8C430023CED42659infoc; b_lsid=BEB8EE7F_18742FF8C2E; bsource=search_baidu; _uuid=DE810E367-B52C-AF6E-A612-EDF4C31567F358591infoc; b_nut=100; buvid_fp=711a632b5c876fa8bbcf668c1efba551; SESSDATA=7624af93%2C1696008331%2C862c8%2A42; bili_jct=141a474ef3ce8cf2fedf384e68f6625d; DedeUserID=3493271303096985; DedeUserID__ckMd5=212a836c164605b7; sid=5h4ruv6o; buvid4=978E9208-13DA-F87A-3DC0-0B8EDF46E80434329-123040301-dWliG5BMrUb70r3g583u7w%3D%3D"
     },
     timeout:5000,
     limit:8,
